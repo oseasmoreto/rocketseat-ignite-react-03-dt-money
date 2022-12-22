686e26e36e7df3ae137f6fe4819b26707c674152
oseasmoreto <oseas_moreto@hotmail.com>
2022-12-22 11:12:14 -0300

    Adiciona estilos do modal de criar transação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 import * as Dialog from '@radix-ui/react-dialog'
 
 import imgLogo from '../../assets/image/logo.svg'
+import { NewTransactionModal } from '../NewTransactionModal'
 
 export function Header() {
   return (
@@ -13,14 +14,7 @@ export function Header() {
           <Dialog.Trigger asChild>
             <NewTransactionButton>Nova transação</NewTransactionButton>
           </Dialog.Trigger>
-          <Dialog.Portal>
-            <Dialog.Overlay>
-              <Dialog.Content>
-                <Dialog.Title>Nova transação</Dialog.Title>
-                <Dialog.Close />
-              </Dialog.Content>
-            </Dialog.Overlay>
-          </Dialog.Portal>
+          <NewTransactionModal />
         </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,28 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import { X } from 'phosphor-react'
+
+import { Close, Content, Overlay } from './styles'
+
+export function NewTransactionModal() {
+  return (
+    <Dialog.Portal>
+      <Overlay>
+        <Content>
+          <Dialog.Title>Nova transação</Dialog.Title>
+
+          <Close>
+            <X size={24} />
+          </Close>
+
+          <form action="">
+            <input type="text" placeholder="Descrição" required />
+            <input type="number" placeholder="Preço" />
+            <input type="text" placeholder="Categoria" />
+
+            <button type="submit">Cadastrar</button>
+          </form>
+        </Content>
+      </Overlay>
+    </Dialog.Portal>
+  )
+}
